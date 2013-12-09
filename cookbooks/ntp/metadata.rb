5c9486fcc8cb0d0d7c5562728cdfa8a7f9e329cf
Andrei Cuşnir <cusnir@gmail.com>
2013-12-09 20:06:01 +0200

    adding ntp recipe

---
+++ cookbooks/ntp/metadata.rb
@@ -0,0 +1,7 @@
+name             'ntp'
+maintainer       'Andrei Cusnir'
+maintainer_email 'cusnir@gmail.com'
+license          'Apache 2.0'
+description      'Installs/Configures ntp'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
